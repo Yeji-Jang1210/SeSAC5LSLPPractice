5064be52ef711e69c46bf09b2e1b85f5fa036e10
Yeji-Jang1210 <jyg8727@gmail.com>
2024-07-23 15:39:07 +0900

    Initial Commit

---
+++ SeSAC5LSLPPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC5LSLPPractice
+//
+//  Created by 장예지 on 7/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
